4fbaa0a66d3dfe7cc928c97c3cdcf685d890b271
Rishi Serumadar <rishi8094@users.noreply.github.com>
2021-09-08 20:31:20 +0100

    Added Get Rate

--- src/index.ts
+++ src/index.ts
@@ -33,5 +33,31 @@ export default class FXMoney {
       this.rates[base] = 1;
     }
   }
-  
+
+  getRate(from: string, to: string, customRates?: FXMoneyRates): number {
+    let rates = customRates || this.rates;
+    // Check if the from currency is valid and exists in the rates.
+    if (!rates[from]) {
+      throw new Error(`Cannot get rate for non existant currency ${from} (from)`);
+    }
+
+    // Check if the to currency is valid and exists in the rates.
+    if (!rates[to]) {
+      throw new Error(`Cannot get rate for non existant currency ${to} (to)`);
+    }
+
+    // Check if the from currency is the same as the base currency.
+    if (from === this.base) {
+      return rates[to];
+    }
+
+    // Check if the to currency is the same as the base currency.
+    if (to === this.base) {
+      return 1 / rates[from];
+    }
+
+    // Otherwise, return the rate between the two currencies. This is calculated by the inverse of the form rate.
+    // relative to the exchange rate of between currencies.
+    return (1 / rates[from]) * rates[to];
+  }
 }

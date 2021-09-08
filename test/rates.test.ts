a27865f53a864f8a416df2466d1c932849dda827
Rishi Serumadar <rishi8094@users.noreply.github.com>
2021-09-08 21:45:16 +0100

    Update rates.test.ts

--- test/rates.test.ts
+++ test/rates.test.ts
@@ -7,7 +7,7 @@ describe('check to see correct rates are returned', () => {
       'USD': 1.38,
       'EUR': 1.16
     });
-    expect(fxmoney.getRate('GBP', 'USD')).toBe(1.38);
+    expect(fxmoney.getRate({from: "GBP", to: "USD"})).toBe(1.38);
   });
 
   it('returns the correct rate for base against base', () => {
@@ -16,7 +16,7 @@ describe('check to see correct rates are returned', () => {
       'USD': 1.38,
       'EUR': 1.16
     });
-    expect(fxmoney.getRate('GBP', 'GBP')).toBe(1);
+    expect(fxmoney.getRate({from: "GBP", to: "GBP"})).toBe(1);
   });
 
   it('returns the rate for 2 non base currencies', () => {
@@ -25,27 +25,27 @@ describe('check to see correct rates are returned', () => {
       'USD': 1.38,
       'EUR': 1.16
     });
-    expect(parseFloat(fxmoney.getRate('EUR', 'USD').toFixed(2))).toBe(1.19);
+    expect(parseFloat(fxmoney.getRate({from: "EUR", to: "USD"}).toFixed(2))).toBe(1.19);
   });
 
   it('correctly adjusts the base rate to be 1', () => {
     let fxmoney = new FXMoney('GBP', {
       'GBP': 1.9
     });
-    expect(fxmoney.getRate('GBP', 'GBP')).toBe(1);
+    expect(fxmoney.getRate({from: "GBP", to: "GBP"})).toBe(1);
   });
 
   it('throws an error for a non existant "from" currency', () => {
     let fxmoney = new FXMoney('GBP', {
       'GBP': 1.9
     });
-    expect(()=>fxmoney.getRate('EUR', 'GBP')).toThrow('Cannot get rate for non existant currency EUR (from)');
+    expect(()=>fxmoney.getRate({from: "EUR", to: "GBP"})).toThrow('Cannot get rate for non existant currency EUR (from)');
   });
 
   it('throws an error for a non existant "to" currency', () => {
     let fxmoney = new FXMoney('GBP', {
       'GBP': 1.9
     });
-    expect(()=>fxmoney.getRate('GBP', 'EUR')).toThrow('Cannot get rate for non existant currency EUR (to)');
+    expect(()=>fxmoney.getRate({from: "GBP", to: "EUR"})).toThrow('Cannot get rate for non existant currency EUR (to)');
   });
 });
